85b5b41170c8e1f9fbcd4e1f2cc54d933b8cac69
Maham-jawaid1 <mahamjawaid5678@gmail.com>
2024-04-19 12:43:20 -0700

    Adding word counter

---
+++ index.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node 
+
+
+//import inquirer module, which is the cammand line interface
+import inquirer from "inquirer";
+//declare a constant answerand assign it to the result of inquirer.prompt function
+const answer :{
+    Sentance: string
+} =  await inquirer.prompt([
+   {
+   name: "Sentance",
+   type: "input",
+   message: "Enter your sentance to count the word:"
+   }
+])
+const words = answer.Sentance.trim().split(" ")
+//print the array of words to the console
+console.log(words);
+//print the word count of the sentance to the console
+console.log(`Your sentance word count is ${words.length}`)
